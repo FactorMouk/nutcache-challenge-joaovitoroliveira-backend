5c12c2ce56afb56f8986f8494cf8a69e19a5401c
FactorMouk <jvoa@cin.ufpe.br>
2021-08-17 19:40:19 -0300

    Adding ESLint and Prettier

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 
 const app = express();
 
 app.listen(8000, () => {
-    console.log('Server running on port 8000!');
+  console.log('Server running on port 8000!');
 });
