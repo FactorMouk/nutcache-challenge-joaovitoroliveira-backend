ae75fa80487e8e01a7807c819055c77a8c44cb81
FactorMouk <jvoa@cin.ufpe.br>
2021-08-17 19:54:04 -0300

    Adding Employee Model

---
+++ src/models/employee.model.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const EmployeeSchema = new Schema({
+  name: { type: String, required: true, max: 100 },
+  birthDate: { type: Date, required: true },
+  gender: { type: String, required: true, max: 30 },
+  email: { type: String, required: true, max: 100 },
+  cpf: { type: String, required: true, max: 14 },
+  startDate: { type: String, required: true, max: 10 },
+  team: { type: String, required: true, max: 15 },
+});
+
+module.exports = mongoose.model('Employee', EmployeeSchema);
