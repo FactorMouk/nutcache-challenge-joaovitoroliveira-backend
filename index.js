922b71b10b5a9feaa1c9a2b211430294faff03c1
FactorMouk <jvoa@cin.ufpe.br>
2021-08-18 16:37:24 -0300

    Hotfix : Adding CORS support

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 // Connects to MongoDB database
 const uri =
@@ -13,6 +14,7 @@ db.on('error', console.error.bind(console, 'MongoDB connection error!'));
 const employees = require('./src/routes/employees.route');
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use('/nutemployee', employees);
 app.listen(8000, () => {
   console.log('Server running on port 8000!');
