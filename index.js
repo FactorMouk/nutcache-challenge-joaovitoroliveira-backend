f81beaef6f6341bc3e4b2a1f6731393f5086f3b1
FactorMouk <jvoa@cin.ufpe.br>
2021-08-20 12:46:07 -0300

    Hotfix

--- index.js
+++ index.js
@@ -15,6 +15,16 @@ const employees = require('./src/routes/employees.route');
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json'
+  );
+  next();
+});
 app.use('/nutemployee', employees);
 app.listen(8000, () => {
   console.log('Server running on port 8000!');
